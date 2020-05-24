da0c303920e5ca2a3306ca1d8b6972306735bc38
Justin Hammond <justin@dynam.ac>
2020-05-24 23:19:25 +0800

    Start Work on a GlobalState Class

---
+++ ozwadmin-main/ozwcore.cpp
@@ -0,0 +1,6 @@
+#include "ozwcore.h"
+
+OZWCore::OZWCore(QObject *parent) : QObject(parent)
+{
+
+}
---
+++ ozwadmin-main/ozwcore.h
@@ -0,0 +1,17 @@
+#ifndef OZWCORE_H
+#define OZWCORE_H
+
+#include <QObject>
+
+class OZWCore : public QObject
+{
+    Q_OBJECT
+public:
+    explicit OZWCore(QObject *parent = nullptr);
+
+signals:
+
+public slots:
+};
+
+#endif // OZWCORE_H
