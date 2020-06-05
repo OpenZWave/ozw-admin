01c96b5a1e0875226ab82b32930f1496f7bc1df4
Justin Hammond <Justin@dynam.ac>
2020-06-05 12:58:25 +0800

    make sure we build a release build

--- scripts/build.sh
+++ scripts/build.sh
@@ -4,9 +4,9 @@
 cd /opt
 git clone https://github.com/OpenZWave/open-zwave.git && cd open-zwave && make -j4 && make install
 cd /opt
-git clone https://github.com/OpenZWave/qt-openzwave.git && cd qt-openzwave && /opt/qt512/bin/qmake -r && make -j4 && make install
+git clone https://github.com/OpenZWave/qt-openzwave.git && cd qt-openzwave && /opt/qt512/bin/qmake -r CONFIG+=release && make -j4 && make install
 cd /opt
-git clone https://github.com/OpenZWave/ozw-admin.git && cd ozw-admin && /opt/qt512/bin/qmake -r && make -j4 && make install
+git clone https://github.com/OpenZWave/ozw-admin.git && cd ozw-admin && /opt/qt512/bin/qmake -r CONFIG+=release && make -j4 && make install
 cd /opt
 #mkdir -p /opt/AppDir/usr/lib/
 #cp /opt/open-zwave/libopenzwave.so* /opt/AppDir/usr/lib/
